02d158849f001f6c27d8ac447f4821868918309d
mhmh4 <86537561+mhmh4@users.noreply.github.com>
2023-11-22 08:47:59 -0500

    Create a layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,26 @@
 function App() {
-  return "<App />";
+  return (
+    <>
+      <div className="mx-auto min-h-screen w-2/3 pt-[15vh]">
+        <div className="flex">
+          <div className="w-1/2 border">
+            <img src={""} alt="img"></img>
+          </div>
+          <div className="w-1/2">
+            <input type="radio" name="selection" />
+            <label>Option 1</label>
+            <input type="radio" name="selection" />
+            <label>Option 2</label>
+            <input type="radio" name="selection" />
+            <label>Option 3</label>
+            <input type="radio" name="selection" />
+            <label>Option 4</label>
+          </div>
+        </div>
+        <button type="submit">Submit</button>
+      </div>
+    </>
+  );
 }
 
 export default App;
