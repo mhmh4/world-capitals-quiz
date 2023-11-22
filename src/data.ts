83dbe39691de983fecc4e63ee9daf9e3bbbb659f
mhmh4 <86537561+mhmh4@users.noreply.github.com>
2023-11-21 19:07:46 -0500

    Create data.ts

---
+++ src/data.ts
@@ -0,0 +1,24 @@
+const data: Object[] = [
+  {
+    country: "Canada",
+    capital: "Ottawa",
+    imageFileName: "toronto.jpg",
+  },
+  {
+    country: "Greenland",
+    capital: "Nuuk",
+    imageFileName: "nuuk.jpg",
+  },
+  {
+    country: "Mexico",
+    capital: "Mexico City",
+    imageFileName: "mexico-city.jpg",
+  },
+  {
+    country: "United States",
+    capital: "Washington, D.C.",
+    imageFileName: "washington-dc.jpg",
+  },
+];
+
+export default data;
