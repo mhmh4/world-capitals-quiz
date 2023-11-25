72c87e9da1eca219426573eed616824eb9ec9f59
mhmh4 <86537561+mhmh4@users.noreply.github.com>
2023-11-25 11:38:46 -0500

    Update more styles

--- src/App.tsx
+++ src/App.tsx
@@ -59,57 +59,63 @@ function App() {
 
   return (
     <>
-      <div className="mx-auto min-h-screen w-[75vw] pt-[10vh]">
-        <div className="text-slate-500">Score: {score}</div>
-        <h1 className="text-xl">What is the capital of {country}?</h1>
-        <div className="flex">
-          <div className="w-2/3">
-            <img className="rounded-xl" src={src} alt="img"></img>
+      <div>
+        <div className="mx-auto min-h-full w-3/4">
+          <div className="my-6 text-center">
+            <div className="text-slate-500">Score: {score}</div>
+            <h1 className="text-xl">What is the capital of {country}?</h1>
           </div>
-          <div className="w-1/3 px-4">
-            {options.map((option, index) => {
-              return (
-                <div
-                  className={clsx(
-                    "my-2 cursor-pointer rounded border-2 border-slate-300 p-2",
-                    {
-                      "bg-green-200": hasSubmitted && option === answer,
-                      "bg-red-200":
-                        hasSubmitted &&
-                        option === selection &&
-                        selection !== answer,
-                      "border-blue-300 bg-blue-50":
-                        !hasSubmitted && option === selection,
-                      "hover:bg-slate-100": !hasSubmitted,
-                    },
-                  )}
-                  key={index}
-                  onClick={() => {
-                    setSelection(option);
-                  }}
+          <div className="flex">
+            <div className="w-2/3">
+              <img className="rounded-xl" src={src} alt="img"></img>
+            </div>
+            <div className="w-1/3 px-4">
+              <div className="flex flex-col">
+                {options.map((option, index) => {
+                  return (
+                    <div
+                      className={clsx(
+                        "mb-2 grow cursor-pointer rounded border-2 border-slate-400 p-2",
+                        {
+                          "bg-green-200": hasSubmitted && option === answer,
+                          "bg-red-200":
+                            hasSubmitted &&
+                            option === selection &&
+                            selection !== answer,
+                          "border-blue-400 bg-blue-50":
+                            !hasSubmitted && option === selection,
+                          "hover:bg-slate-100": !hasSubmitted,
+                        },
+                      )}
+                      key={index}
+                      onClick={() => {
+                        setSelection(option);
+                      }}
+                    >
+                      {option}
+                    </div>
+                  );
+                })}
+              </div>
+              {hasSubmitted ? (
+                <button
+                  className="mt-3 w-1/2 rounded border bg-slate-200 py-1 hover:bg-slate-300"
+                  type="button"
+                  onClick={reset}
                 >
-                  {option}
-                </div>
-              );
-            })}
-            {hasSubmitted ? (
-              <button
-                className="mt-3 w-1/2 rounded border bg-slate-200 py-1 hover:bg-slate-300"
-                type="button"
-                onClick={reset}
-              >
-                Next
-              </button>
-            ) : (
-              <button
-                disabled={selection === ""}
-                className="mt-3 w-1/2 rounded border bg-slate-200 py-1 hover:bg-slate-300"
-                type="submit"
-                onClick={handleSubmit}
-              >
-                Submit
-              </button>
-            )}
+                  Next
+                </button>
+              ) : (
+                <button
+                  disabled={selection === ""}
+                  className="mt-3 w-1/2 rounded border bg-slate-200 py-1 hover:bg-slate-300"
+                  type="submit"
+                  onClick={handleSubmit}
+                >
+                  Submit
+                </button>
+              )}
+            </div>
           </div>
         </div>
       </div>
